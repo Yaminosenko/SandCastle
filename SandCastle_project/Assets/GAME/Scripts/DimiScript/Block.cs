016b990ccf851b16b905258fcbd78d66b39c9a8e
Dimitry <xxhellfire69xx@gmail.com>
2020-05-21 16:49:17 +0200

    Update:GridManager

--- SandCastle_project/Assets/GAME/Scripts/DimiScript/Block.cs
+++ SandCastle_project/Assets/GAME/Scripts/DimiScript/Block.cs
@@ -5,33 +5,94 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     public Transform[] offset = new Transform[4];
+    public GameObject[] blockAdjacent = new GameObject[8];
     public LayerMask layerMask;
+    public LayerMask blockMask;
+
+    public float angleSloap;
+    public float positionY;
+    public int test;
 
 
-    private float[] offsetDistance = new float[4]; 
+    public float[] offsetDistance = new float[4]; 
 
-    private void OnEnable()
+    private void Awake()
     {
+    }
+
+    private void Start()
+    {
+        SetRotationBlock();
+       
+    }
+
+    private void Update()
+    {
+        GetAdjacentBlock();
+    }
 
 
+    private void SetRotationBlock()
+    {
         for (int i = 0; i < offset.Length; i++)
         {
             RaycastHit hit;
             if (Physics.Raycast(offset[i].transform.position, offset[i].transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity, layerMask))
             {
-                //Debug.DrawRay(offset[i].transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
-                //Debug.Log("Did Hit");
+                // Debug.DrawRay(offset[i].transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow, Mathf.Infinity);
+                Debug.Log("Did Hit");
 
                 offsetDistance[i] = hit.distance;
 
             }
             else
             {
-              
+                offsetDistance[i] = 10000f;
             }
         }
 
+        if (offsetDistance[0] == offsetDistance[1] && offsetDistance[1] == offsetDistance[2] && offsetDistance[2] == offsetDistance[3] && offsetDistance[3] == offsetDistance[0])
+        {
 
+        }
+        else if (offsetDistance[0] > offsetDistance[3])
+        {
+            transform.rotation = Quaternion.Euler(angleSloap, transform.rotation.y, transform.rotation.z);
+            test = 1;
+        }
+
+        else if (offsetDistance[3] > offsetDistance[0])
+        {
+            transform.rotation = Quaternion.Euler(-angleSloap, transform.rotation.y, transform.rotation.z);
+            test = 2;
+        }
+
+        else if (offsetDistance[1] > offsetDistance[2])
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, angleSloap);
+            test = 3;
+        }
+
+        else if (offsetDistance[2] > offsetDistance[1])
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, -angleSloap);
+            test = 4;
+        }
+
+        //else if (offsetDistance[1] >= offsetDistance[2])
+        //    transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, -angleSloap);
+
+        transform.position = new Vector3(transform.position.x, positionY, transform.position.z);
+    }
+
+    public void GetAdjacentBlock()
+    {
+        Collider[] AdjacentBlock = Physics.OverlapSphere(transform.position, 1, blockMask);
+
+        for (int i = 0; i < AdjacentBlock.Length; i++)
+        {
+            blockAdjacent[i] = AdjacentBlock[i].gameObject; 
+        }
     }
 }
 
--- SandCastle_project/Assets/GAME/Scripts/DimiScript/GridSpawner.cs
+++ SandCastle_project/Assets/GAME/Scripts/DimiScript/GridSpawner.cs
@@ -9,7 +9,9 @@ public class GridSpawner : MonoBehaviour
     public int worldWidth = 10;
     public int worldHeight = 10;
     public float multiply = 1f;
+    public float angleSlop = 20;
     public LayerMask layerMask;
+    private List<GameObject> allBlock = new List<GameObject>();
 
     //public float spawnSpeed = 0.2f;
 
@@ -24,21 +26,29 @@ public class GridSpawner : MonoBehaviour
 
                 GameObject block = Instantiate(blockPrefab, Vector3.zero, blockPrefab.transform.rotation) as GameObject;
                 block.transform.parent = transform;
+                block.GetComponent<Block>().angleSloap = angleSlop;
                 block.transform.localPosition = new Vector3(x * multiply, 0, z * multiply);
 
                 RaycastHit hit;
                 if (Physics.Raycast(block.transform.position, block.transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity, layerMask))
                 {
                     Debug.DrawRay(block.transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
-                    Debug.Log("Did Hit");
+                    //Debug.Log("Did Hit");
+                    block.GetComponent<Block>().positionY = hit.point.y;
+                    allBlock.Add(block);
                 }
                 else
                 {
                     Destroy(block);
                 }
-
-
             }
+            foreach (GameObject block in allBlock)
+            {
+                block.GetComponent<Block>().GetAdjacentBlock();
+            }
+
+            allBlock.Clear();
+            
         }
     }
 }
