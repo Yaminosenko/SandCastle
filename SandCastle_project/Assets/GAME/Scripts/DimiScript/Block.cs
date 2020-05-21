7c674b47fb12d4c0189c2524a46e20b09bc98564
Dimitry <xxhellfire69xx@gmail.com>
2020-05-21 13:44:24 +0200

    Add: Grid_Spawn_test

---
+++ SandCastle_project/Assets/GAME/Scripts/DimiScript/Block.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public Transform[] offset = new Transform[4];
+    public LayerMask layerMask;
+
+
+    private float[] offsetDistance = new float[4]; 
+
+    private void OnEnable()
+    {
+
+
+        for (int i = 0; i < offset.Length; i++)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(offset[i].transform.position, offset[i].transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity, layerMask))
+            {
+                //Debug.DrawRay(offset[i].transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
+                //Debug.Log("Did Hit");
+
+                offsetDistance[i] = hit.distance;
+
+            }
+            else
+            {
+              
+            }
+        }
+
+
+    }
+}
+
---
+++ SandCastle_project/Assets/GAME/Scripts/DimiScript/GridSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSpawner : MonoBehaviour
+{
+    public GameObject blockPrefab;
+
+    public int worldWidth = 10;
+    public int worldHeight = 10;
+    public float multiply = 1f;
+    public LayerMask layerMask;
+
+    //public float spawnSpeed = 0.2f;
+
+    void Start()
+    {
+        for (int x = 0; x < worldWidth; x++)
+        {
+            for (int z = 0; z < worldHeight; z++)
+            {
+               Vector3 test = new Vector3(x * multiply, 0, z * multiply);
+
+
+                GameObject block = Instantiate(blockPrefab, Vector3.zero, blockPrefab.transform.rotation) as GameObject;
+                block.transform.parent = transform;
+                block.transform.localPosition = new Vector3(x * multiply, 0, z * multiply);
+
+                RaycastHit hit;
+                if (Physics.Raycast(block.transform.position, block.transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity, layerMask))
+                {
+                    Debug.DrawRay(block.transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
+                    Debug.Log("Did Hit");
+                }
+                else
+                {
+                    Destroy(block);
+                }
+
+
+            }
+        }
+    }
+}
