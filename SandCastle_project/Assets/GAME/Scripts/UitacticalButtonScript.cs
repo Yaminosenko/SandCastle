23445576459144a46fcd063736cdfc2ea4a18cb8
polskonexion69 <48548145+polskonexion69@users.noreply.github.com>
2020-06-11 17:41:27 +0200

    Add : beaucoup UI

---
+++ SandCastle_project/Assets/GAME/Scripts/UitacticalButtonScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UitacticalButtonScript : MonoBehaviour
+{
+    public GameObject shotPanel;
+    public GameObject trapPanel;
+    public GameObject invisiblePanel;
+    public GameObject hackPanel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shotPanel.SetActive(false);
+        trapPanel.SetActive(false);
+        invisiblePanel.SetActive(false);
+        hackPanel.SetActive(false);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Shot()
+    {
+        shotPanel.SetActive(true);
+        trapPanel.SetActive(false);
+        invisiblePanel.SetActive(false);
+        hackPanel.SetActive(false);
+       
+    }
+
+    public void Trap()
+    {
+        shotPanel.SetActive(false);
+        trapPanel.SetActive(true);
+        invisiblePanel.SetActive(false);
+        hackPanel.SetActive(false);
+    }
+
+    public void Invisible()
+    {
+        shotPanel.SetActive(false);
+        trapPanel.SetActive(false);
+        invisiblePanel.SetActive(true);
+        hackPanel.SetActive(false);
+    }
+
+    public void Hack()
+    {
+        shotPanel.SetActive(false);
+        trapPanel.SetActive(false);
+        invisiblePanel.SetActive(false);
+        hackPanel.SetActive(true);
+    }
+
+}
